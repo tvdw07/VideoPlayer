8a292be918436a1841d134cd5965a0b46bb9a5c5
Ghoastplayer <tim.vonderweppen@web.de>
2026-02-03 15:48:01 +0100

    Handle additional exceptions in password verification for improved security

--- videoplayer/security.py
+++ videoplayer/security.py
@@ -3,7 +3,8 @@ from flask import current_app, request
 from flask_login import current_user
 from flask import redirect, url_for
 from argon2 import PasswordHasher
-from argon2.exceptions import VerifyMismatchError
+from argon2.exceptions import VerifyMismatchError, InvalidHashError, InvalidHash
+
 
 def auth_required(view):
     @wraps(view)
@@ -28,4 +29,10 @@ def verify_password(password_hash: str, password: str) -> bool:
     try:
         return _ph.verify(password_hash, password)
     except VerifyMismatchError:
+        return False
+    except (InvalidHashError, InvalidHash):
+        #Add logging here
+
+
+        # Invalid hash format
         return False
