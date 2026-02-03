87f0457dca576faf035034a7c5cd62a7c23024a2
Ghoastplayer <tim.vonderweppen@web.de>
2026-02-03 16:10:55 +0100

    Improve password hashing and verification with error handling and logging

--- videoplayer/security.py
+++ videoplayer/security.py
@@ -3,8 +3,10 @@ from flask import current_app, request
 from flask_login import current_user
 from flask import redirect, url_for
 from argon2 import PasswordHasher
-from argon2.exceptions import VerifyMismatchError, InvalidHashError, InvalidHash
+from argon2.exceptions import VerifyMismatchError, InvalidHashError, InvalidHash, HashingError
+from .logging import setup_logging
 
+logger = setup_logging()
 
 def auth_required(view):
     @wraps(view)
@@ -23,7 +25,10 @@ def auth_required(view):
 _ph = PasswordHasher()
 
 def hash_password(password: str) -> str:
-    return _ph.hash(password)
+    try:
+        return _ph.hash(password)
+    except HashingError as e:
+        raise RuntimeError("Password hashing failed +due to an internal error") from e
 
 def verify_password(password_hash: str, password: str) -> bool:
     try:
@@ -31,8 +36,6 @@ def verify_password(password_hash: str, password: str) -> bool:
     except VerifyMismatchError:
         return False
     except (InvalidHashError, InvalidHash):
-        #Add logging here
-
-
+        logger.warning("Password verification failed due to invalid hash format")
         # Invalid hash format
         return False
